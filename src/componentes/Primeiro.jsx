f3399dc2868342f096cba939c9049a22be5f3a57
Airton Oliveira <airton.ddk@hotmail.com>
2019-04-03 15:07:40 -0300

    aula react

---
+++ src/componentes/Primeiro.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function(){
+    return <h1>Primeiro</h1>;
+}
+
+//Código usando função Arrow:
+//export default () => (<h1>Primeiro</h1>);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Primeiro from './componentes/Primeiro';
+import DefaultDoReact from './componentes/default-react/App';
+import BomDia from './componentes/BomDia';
+import Multi, {BoaNoite} from './componentes/Multiplos';
+import Saudacao from './componentes/Saudacao';
+import Pai from './componentes/Pai';
+import Filho from './componentes/Filho'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+ReactDOM.render(
+    <div>
+        <Pai nome="Paulo" sobrenome="Silva" >        
+            <Filho nome="Paulo" />
+            <Filho nome="Carla" />
+            <Filho nome="Pedro" />
+        </Pai>
+    </div>
+, document.getElementById('root'));
+
+
+
+//Componentes devem ter nome maiúsculo
+//ReactDOM.render(<BomDia nome="Teste"/>, document.getElementById('root'));
+/*
+
+ReactDOM.render(
+    <div>
+        <Saudacao tipo="Bom dia" nome="João" />
+    </div>
+, document.getElementById('root'));
+
+ReactDOM.render(
+    <div>
+        <Multi.BoaTarde nome="Ana"/>
+        <BoaNoite nome="João"/>
+    </div>
+    , document.getElementById('root'));
+*/
